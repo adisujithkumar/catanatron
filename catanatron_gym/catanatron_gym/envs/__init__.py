63b6cd8318ee9e3da89a306abff8fe205451ad3c
adisujithkumar <32069166+adisujithkumar@users.noreply.github.com>
2022-11-28 02:17:38 -0800

    Update init to include register call

--- catanatron_gym/catanatron_gym/envs/__init__.py
+++ catanatron_gym/catanatron_gym/envs/__init__.py
@@ -1,1 +1,8 @@
 from catanatron_gym.envs.catanatron_env import CatanatronEnv
+
+from gym.envs.registration import register
+
+register(
+    id="catanatron-v0",
+    entry_point="catanatron_gym.envs:CatanatronEnv",
+)
